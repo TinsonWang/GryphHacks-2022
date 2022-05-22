980db24e2d72b0e30c48fa8f07b0e25c8c478634
TinsonWang <tinsonwang@gmail.com>
2022-05-22 16:28:11 -0400

    added comments for backend code

--- Server/database.go
+++ Server/database.go
@@ -9,8 +9,7 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
-// use godot package to load/read the .env file and
-// return the value of the key
+// use godot package to load/read the .env file and return value of key
 func goDotEnvVariable(key string) string {
 
     // load .env file
@@ -23,7 +22,10 @@ func goDotEnvVariable(key string) string {
     return os.Getenv(key)
   }
 
+//Registers a new user using the provided login and password
 func insertDatabase(user_login string, user_password string) {
+
+    //Load in variables
     host := goDotEnvVariable("HOST")
     database := goDotEnvVariable("DATABASE")
     user := goDotEnvVariable("DB_USER")
@@ -32,24 +34,22 @@ func insertDatabase(user_login string, user_password string) {
     // fmt.Printf("Host: %s\nDatabase: %s\nUser: %s\nPassword: %s\n", host, database, user, password)
     // fmt.Printf("LOGIN: %s\nPASSWORD: %s\n", user_login, user_password)
 
+    //Set up string variable
     var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
     // fmt.Printf("CONNECTIONSTRING: %s\n", connectionString)
 
-    // // Open up our database connection.
-    // // I've set up a database on my local machine using phpmyadmin.
-    // // The database is called testDb
+    //Establish database connection.
     db, err := sql.Open("mysql", connectionString)
 
-    // // if there is an error opening the connection, handle it
+    //if there is an error opening the connection, handle it
     if err != nil {
         panic(err.Error())
     }
 
-    // // defer the close till after the main function has finished
-    // // executing
+    //Close connection after finishing
     defer db.Close()
 
-    // // perform a db.Query insert
+    //Prepare a query and perform an INSERT query
     query := fmt.Sprintf(`INSERT INTO parkit (username, password, qrcode) VALUES ('%s', '%s', 1)`, user_login, user_password)
     insert, err := db.Query(query)
 
@@ -57,68 +57,81 @@ func insertDatabase(user_login string, user_password string) {
     if err != nil {
         panic(err.Error())
     }
-    // // be careful deferring Queries if you are using transactions
+
+    //Close after finishing
     defer insert.Close()
 }
 
+//Upon a valid login, flips the QR code status in the database, indicating active or inactive QR code
 func flipQRCode(user_login string, user_password string)(rowCounts int) {
+
+    //Load in variables
     host := goDotEnvVariable("HOST")
     database := goDotEnvVariable("DATABASE")
     user := goDotEnvVariable("DB_USER")
     password := goDotEnvVariable("DB_PASSWORD")
+
+    //Set up string variable
     var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
 
-    // // Open up database connection.
+    //Establish database connection.
     db, err := sql.Open("mysql", connectionString)
 
-    // // if there is an error opening the connection, handle it
+    //if there is an error opening the connection, handle it
     if err != nil {
         panic(err.Error())
     }
 
-    // // defer the close till after the main function has finished
+    //Close connection after finishing
     defer db.Close()
 
-    // // perform a db.Query insert
+    //Prepare and perform a SELECT query
     query := fmt.Sprintf(`SELECT qrcode FROM parkit WHERE username='%s' AND password='%s'`, user_login, user_password)
     fmt.Printf(query)
     rows, err := db.Query(query)
 
-    // // if there is an error inserting, handle it
+    //if there is an error inserting, handle it
     if err != nil {
         panic(err.Error())
     }
 
-    // // be careful deferring Queries if you are using transactions
+    //Close after finishing
     defer rows.Close()
 
+    //Tracker to determine if login success or failure
     rowCounts = 0
-
     for rows.Next() {
         rowCounts++
         var qrcode int
         if err := rows.Scan(&qrcode); err != nil {
                 log.Fatal(err)
         }
+        //Flip qrcode status based on its current value
         if (qrcode == 1) {
+
+            //Prepare and perform an UPDATE query
             query2 := fmt.Sprintf(`UPDATE parkit SET qrcode=0 WHERE username='%s'`, user_login)
             update, err2 := db.Query(query2)
-            // // if there is an error inserting, handle it
+
+            //if there is an error inserting, handle it
             if err2 != nil {
                 panic(err.Error())
             }
 
-            // // be careful deferring Queries if you are using transactions
+            //Close after finishing
             defer update.Close()
         } else {
+
+            //Prepare and perform an UPDATE query
             query2 := fmt.Sprintf(`UPDATE parkit SET qrcode=1 WHERE username='%s'`, user_login)
             update, err2 := db.Query(query2)
-            // // if there is an error inserting, handle it
+
+            //if there is an error inserting, handle it
             if err2 != nil {
                 panic(err.Error())
             }
 
-            // // be careful deferring Queries if you are using transactions
+            //Close after finishing
             defer update.Close()
         }
     }
@@ -127,5 +140,6 @@ func flipQRCode(user_login string, user_password string)(rowCounts int) {
             log.Fatal(err)
     }
 
+    //Return the value of rowCounts for the handler to determine login status
     return
 }
--- Server/server.go
+++ Server/server.go
@@ -17,15 +17,15 @@ type User struct {
   Password string
 }
 
+// Handler for landing page
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-
   fmt.Print("Welcome to ParkIt! Powered by FireTrucks.")
-
 }
 
 // This handler takes a login-password pair from the request object, finds it in the database, and flips the database column qrcode value
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 
+  //First parse the request object
   var some_user User
   err := json.NewDecoder(r.Body).Decode(&some_user)
   if err != nil {
@@ -34,6 +34,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
   }
   fmt.Fprintf(w, "POST request successful\n")
   fmt.Fprintf(w, "Login: %s\nPassword: %s\n", some_user.Login, some_user.Password)
+
+  //Depending on login success or failure, prepare a response object
   rowCounts := flipQRCode(some_user.Login, some_user.Password)
   w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
@@ -47,13 +49,16 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if err2 != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err2)
 	}
+
+  //Send response object
 	w.Write(jsonResp)
 	return
-
 }
 
 // This handler takes a login-password pair from the request object and adds it to the database
 func regHandler(w http.ResponseWriter, r *http.Request) {
+
+  //First parse the request object
   var some_user User
   err := json.NewDecoder(r.Body).Decode(&some_user)
   if err != nil {
@@ -62,11 +67,13 @@ func regHandler(w http.ResponseWriter, r *http.Request) {
   }
   fmt.Fprintf(w, "POST request successful\n")
   fmt.Fprintf(w, "Login: %s\nPassword: %s\n", some_user.Login, some_user.Password)
-  insertDatabase(some_user.Login, some_user.Password)
 
+  //Call SQL function using data from request object
+  insertDatabase(some_user.Login, some_user.Password)
 }
 
 func downloadFile(URL, fileName string) error {
+
   //Get the response bytes from the url
   response, err := http.Get(URL)
   if err != nil {
@@ -84,7 +91,7 @@ func downloadFile(URL, fileName string) error {
   }
   defer file.Close()
 
-  //Write the bytes to the fiel
+  //Write the bytes to the file
   _, err = io.Copy(file, response.Body)
   if err != nil {
     return err
@@ -94,23 +101,28 @@ func downloadFile(URL, fileName string) error {
 }
 
 func qrHandler(w http.ResponseWriter, r *http.Request) {
-    min := 10000000000
-    max := 30000000000
-    rand := rand.Intn(max - min) + min
-    url := "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + string(rand)
-    resp, err := http.Get(url);
-    if err != nil {
-        log.Fatalln(err)
-    }
-    fmt.Printf("%+v\n", resp)
 
-    fileName := "test.png"
-    file := downloadFile(url, fileName)
-    if file != nil {
-      log.Fatal(file)
-    }
-    fmt.Printf("File %s download in current working directory", fileName)
-    fileBytes, err := ioutil.ReadFile("test.png")
+  //Generate a random string for the URL
+  min := 10000000000
+  max := 30000000000
+  rand := rand.Intn(max - min) + min
+  url := "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + string(rand)
+  resp, err := http.Get(url);
+  if err != nil {
+      log.Fatalln(err)
+  }
+  fmt.Printf("%+v\n", resp)
+
+  //Fetch the QR code image
+  fileName := "test.png"
+  file := downloadFile(url, fileName)
+  if file != nil {
+    log.Fatal(file)
+  }
+  fmt.Printf("File %s download in current working directory", fileName)
+
+  //Upload the QR code image
+  fileBytes, err := ioutil.ReadFile("test.png")
 	if err != nil {
 		panic(err)
 	}
@@ -120,16 +132,15 @@ func qrHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func mapHandler(w http.ResponseWriter, r *http.Request) {
-}
-
 func main() {
+
+  //Designate handlers for each endpoint
   http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/register", regHandler)
 	http.HandleFunc("/qr", qrHandler)
-	http.HandleFunc("/map", mapHandler)
 
+  //Start server
 	fmt.Print("Starting server on port 5000\n")
 	if err := http.ListenAndServe(":5000", nil); err != nil {
         log.Fatal(err)
